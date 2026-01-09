b4b15aae417a379c765458d552518f90d5323228
cesarcm590 <cesar.carrillo@ingenieria.unam.edu>
2026-01-09 14:19:40 -0600

    Ready for Netlify

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'El Vlog de un pelado';
+export const SITE_DESCRIPTION = 'Bienvenidos a este espacio que tiene como intención mostarme un poco más a fondo, información que puede resultad valiosa o no...';
